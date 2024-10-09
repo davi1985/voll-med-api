661bb5cd0e9a0962478503e6e17899629f1b4887
Davi Silva <davisilvaphoto@gmail.com>
2024-10-09 14:04:18 -0300

    add uri and response entity in findById method

--- src/main/java/dev/dsilva/api/vollmed/controller/DoctorController.java
+++ src/main/java/dev/dsilva/api/vollmed/controller/DoctorController.java
@@ -1,7 +1,6 @@
 package dev.dsilva.api.vollmed.controller;
 
 import dev.dsilva.api.vollmed.doctors.*;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +9,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/doctors")
 public class DoctorController {
-    private final static int PAGINATION_SIZE = 10;
+
     private final static String SORT_PROPERTY = "name";
 
     @Autowired
@@ -22,22 +24,34 @@ public class DoctorController {
 
     @PostMapping
     @Transactional
-    public void save(@RequestBody @Valid CreateDoctorRequest createDoctorRequest) {
-        repository.save(new Doctor(createDoctorRequest));
+    public ResponseEntity<UpdateDoctorResponse> save(@RequestBody @Valid CreateDoctorRequest createDoctorRequest,
+                                                     UriComponentsBuilder builder) {
+        var doctor = new Doctor(createDoctorRequest);
+        repository.save(doctor);
+
+        var uri = builder
+                .path(("/doctors/{id}"))
+                .buildAndExpand(doctor.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(new UpdateDoctorResponse(doctor));
     }
 
     @GetMapping
-    public ResponseEntity<Page<DoctorResponse>> list(@PageableDefault(size = PAGINATION_SIZE, sort = {SORT_PROPERTY}) Pageable pagination) {
+    public ResponseEntity<Page<DoctorResponse>> list(@PageableDefault(sort = SORT_PROPERTY) Pageable pagination) {
         var page = repository.findAll(pagination).map(DoctorResponse::new);
 
         return ResponseEntity.ok(page);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DoctorData> findById(@PathVariable Long id) {
+    public ResponseEntity<Optional<DoctorData>> findById(@PathVariable Long id) {
         var doctorData = repository.findById(id)
-                .map(DoctorData::new)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("Doctor with id %d not found", id)));
+                .map(DoctorData::new);
+
+        if (doctorData.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
 
         return ResponseEntity.ok(doctorData);
     }
